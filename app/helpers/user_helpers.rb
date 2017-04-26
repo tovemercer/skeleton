750f34bd6bf203cfcc2b6dd43e6e792ee172a659
Tove N Mercer <email": null>
2017-04-26 18:13:09 -0500

    user helpers added

---
+++ app/helpers/user_helpers.rb
@@ -0,0 +1,12 @@
+def current_user
+  @current_user ||= User.find_by(id: session[:user_id])
+end
+
+def logged_in?
+  session[:user_id] != nil
+end
+
+# authenticates user
+def check_login
+  redirect "/sessions/new" unless logged_in?
+end
